0cb82a204c42308a5b61b6c481c14b1ed2575725
Gabriel Ortiz <gabriel.j.ortiz@gmail.com>
2018-02-09 12:08:49 -0800

    When two and three targets five, indices are one and two

--- TwoSum/TwoSum/TwoSum/TwoSum.cs
+++ TwoSum/TwoSum/TwoSum/TwoSum.cs
@@ -8,7 +8,22 @@ namespace TwoSum
         {
             if (numbers == null) throw new ArgumentNullException();
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var firstNumber = numbers[i];
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    var secondNumber = numbers[j];
+                    if (NumbersAddUpToTarget(target, firstNumber, secondNumber))
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
             return new[] { 0, 1 };
         }
+
+        private static bool NumbersAddUpToTarget(int target, int firstNumber, int secondNumber) => firstNumber + secondNumber == target;
     }
 }
